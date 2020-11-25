061fc1cd7c462e7ab3506d55780364c8f6055446
Falk 'DGhost' Ahlendorf <falk.ahlendorf@googlemail.com>
2020-11-25 12:56:44 +0100

    Implementation for the constant color and save functionality

--- main.c
+++ main.c
@@ -11,9 +11,14 @@
 
 #define ELEMENT_COUNT(X) (sizeof(X) / sizeof (X[0]))
 
+enum {
+    saveTimeOut = 2 * 400 //Press save for about 2 sec
+};
+
 static uint8_t leds[660];
 static uint8_t noisePoints[2][22];
 static uint8_t ctrlPoints[ELEMENT_COUNT(noisePoints[0])];
+static volatile uint16_t save;
 static struct Settings * settings;
 
 
@@ -98,22 +103,87 @@ static void sendLeds( void )
     }
 }
 
+
+static void setAllLedsToColor(struct Color const * const color)
+{
+    cli();
+
+    for(unsigned i = 0; i< ELEMENT_COUNT(leds); ++i) {
+         sendColor(color);
+    }
+
+    sei();
+}
+
+static inline void inc(uint8_t * value) {
+    if(*value < 255) ++(*value);
+}
+
+static inline void dec(uint8_t * value) {
+    if(*value > 0) --(*value);
+}
+
+
 static void handleKeyBoard( void )
 {
-    if(isKeyPressed(KEY_1)/* && isKeyToggled(KEY_1) */) {
-        settings->mode = 0;
-    }
+    if(save > 0) { //User Input is disabled during save process.
+        if(isKeyPressed(KEY_1)/* && isKeyToggled(KEY_1) */) {
+            settings->mode = 0;
+        }
 
-    if(isKeyPressed(KEY_2) /* && isKeyToggled(KEY_2) */) {
-        settings->mode = 1;
-    }
+        if(isKeyPressed(KEY_2) /* && isKeyToggled(KEY_2) */) {
+            settings->mode = 1;
+        }
 
-    if(isKeyPressed(KEY_3) /* && isKeyToggled(KEY_3) */) {
-        settings->mode = 2;
-    }
+        if(isKeyPressed(KEY_3) /* && isKeyToggled(KEY_3) */) {
+            settings->mode = 2;
+        }
 
-    if(isKeyPressed(KEY_5) /* && isKeyToggled(KEY_5) */) {
-        settings->mode = 3;
+        if(isKeyPressed(KEY_5) /* && isKeyToggled(KEY_5) */) {
+            settings->mode = 3;
+        }
+
+        if(isKeyPressed(KEY_13)) {
+            settings->mode = 9;
+        }
+
+        if(isKeyPressed(KEY_14)) {
+            settings->mode = 10;
+        }
+
+        if(isKeyPressed(KEY_15)) {
+            settings->mode = 11;
+        }
+
+        if(isKeyPressed(KEY_16)) {
+            settings->mode = 12;
+        }
+
+        if(isKeyPressed(KEY_12)) {
+            switch (settings->mode) {
+            case 9: inc(&settings->color.r); break;
+            case 10: inc(&settings->color.g); break;
+            case 11: inc(&settings->color.b); break;
+            case 12: inc(&settings->color.r); inc(&settings->color.g); inc(&settings->color.b); break;
+            default: break;
+            }
+        }
+
+        if(isKeyPressed(KEY_8)) {
+            switch (settings->mode) {
+            case 9: dec(&settings->color.r); break;
+            case 10: dec(&settings->color.g); break;
+            case 11: dec(&settings->color.b); break;
+            case 12: dec(&settings->color.r); dec(&settings->color.g); dec(&settings->color.b); break;
+            default: break;
+            }
+        }
+
+        if(isKeyPressed(KEY_4)) {
+            if(save > 0) --save;
+        } else {
+            save = saveTimeOut;
+        }
     }
 }
 
@@ -125,8 +195,9 @@ ISR (TIMER0_OVF_vect)
 
 int main( void )
 {
-    uint8_t offset = 0;
+    uint8_t offset = 0;  
 
+    save     = saveTimeOut;
     settings = loadSettings();
 
     if(!settings) {
@@ -143,14 +214,27 @@ int main( void )
     
 
     while(true) {
-        computeCtrlPoints(offset);
-        computeLeds();
-        sendLeds();
-        ++offset;
 
-        if(offset == 0) {
-            moveNoisePoints();
+        if(settings->mode < 9) {
+            computeCtrlPoints(offset);
+            computeLeds();
+            sendLeds();
+            ++offset;
+
+            if(offset == 0) {
+                moveNoisePoints();
+            }
+        } else {
+             setAllLedsToColor(&settings->color);
         }
+
+        if(0 == save) {
+            struct Color color = {.r = 0, .g = 0, .b = 0};
+            setAllLedsToColor(&color); //Blank all LEDs to Signal Save in Progress
+            saveSettings();
+            save = saveTimeOut;
+        }
+
         //__builtin_avr_delay_cycles(500000);
     }
     return 0;
